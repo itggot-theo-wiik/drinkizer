48d00a67c06e21c95e6acb8b037521e566543b2c
itggot-theo-wiik <theo.wiik@itggot.se>
2017-05-31 21:02:52 +0200

    0.001

--- site/js/script.js
+++ site/js/script.js
@@ -1,21 +1,26 @@
 function bg_color() {
     var random_number = Math.random();
 
-    console.log(random_number);
-
-    element = document.querySelector(".changg");
-    console.log(element)
-    element.classList.add(".cyan");
-
-    // if (random_number < 0.33) {
-    //     console.log("Litet");
-    // }
-    // else if (0.33 < random_number && random_number < 0.66) {
-    //     console.log("mellan");
-    // }
-    // else {
-    //     console.log("största!!");
-    // }
+    if (random_number < 0.33) {
+        bg_color_adder("cyan")
+    } else if (0.33 < random_number && random_number < 0.66) {
+        bg_color_adder("pink")
+    } else {
+        bg_color_adder("purple")
+    }
 }
 
-window.onload = bg_color();
+function bg_color_adder(color) {
+    element = document.querySelector("main");
+    element.classList.add(color);
+    element = document.querySelector("header");
+    element.classList.add(color);
+}
+
+var drinks = ["Sprite", "Sprite Zero", "Powerking", "Zingo", "Zingo Passion", "Dr Pepper", "Mountain Dew Burk" , "Mountain Dew Flaska", "Trocadero Burk", "Trodacera Flaska", "Aloe Vera", "Vatten", "Eget val"];
+
+function which_drink() {
+    var random = Math.floor(Math.random() * (drinks.length + 1));
+    // alert("Du fick drickan: " + drinks[random])
+    $('.results').html(drinks[random]);
+}
